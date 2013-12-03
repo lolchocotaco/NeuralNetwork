85257b4346211472745d6ba06e4d2907464272eb
Sameer <schauhan19@gmail.com>
2013-12-03 10:43:00 -0500

    Starting parsing of initial Neural Net

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
+import os.path
+from nnet import nnet
 
-
-def getFile():
-    pass
+def getFile(prompt):
+    fileName = raw_input(prompt)
+    while not os.path.isfile(fileName):
+        print("\tPlease input a valid file")
+        fileName = raw_input(prompt)
+    return fileName
 
 
 def runNet():
     pass
 
-if __name__ == "main":
+if __name__ == "__main__":
+    # netLoc = getFile("Initial net location: ")
+    # trainLoc = getFile("Training Set: ")
+    # outName = raw_input("Output Name: ")
+
+    # epoch = raw_input("Epoch: ")
+    # while not epoch.isdigit():
+    #     epoch = raw_input("\tEnter Integer value for Epoch: ")
+    # epoch = int(epoch)
+
+    # rate = float(raw_input("Rate: "))
+
+    netLoc = "res/sample.NNWDBC.init"
+    trainLoc = "res/wdbc.train"
+    outName = "res/test.out"
+    epoch = "10"
+    rate = "2.1"
+
+    net = nnet(netLoc)
+    print(net.nodesIn)
+    print(net.nodesH)
+    print(net.nodesOut)
     runNet()
--- nnet.py
+++ nnet.py
@@ -2,8 +2,20 @@
 
 
 class nnet:
-    def __init(self):
-        pass
+
+    def __init__(self, netLoc):
+        self.fileName = netLoc
+        self.nodesIn = 0
+        self.nodesH = 0
+        self.nodesOut = 0
+        self.loadFile()
 
     def loadFile(self):
-        pass
+        with open(self.fileName,'r') as f:
+            fLine = (f.readline()).split(" ")
+            self.nodesIn = fLine[0]
+            self.nodesH = fLine[1]
+            self.nodesOut = fLine[2]
+
+
+
